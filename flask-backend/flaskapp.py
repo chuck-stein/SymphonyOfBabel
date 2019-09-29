f45e119bad4205ef369dc36bcdbddc0108d0e33f
chuck-stein <cfgstein@gmail.com>
2019-09-29 13:18:34 -0400

    create react app

--- flask-backend/flaskapp.py
+++ flask-backend/flaskapp.py
@@ -19,11 +19,11 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('index.html', posts=posts)
 
-@app.route('/about')
-def about():
-    return render_template('about.html', title='About')
+# @app.route('/about')
+# def about():
+#     return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
