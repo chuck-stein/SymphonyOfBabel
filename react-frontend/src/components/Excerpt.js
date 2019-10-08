5436316733843d6c52fddf9fec5a544e342a3108
chuck-stein <cfgstein@gmail.com>
2019-10-07 23:04:34 -0400

    add useEffect import

--- react-frontend/src/components/Excerpt.js
+++ react-frontend/src/components/Excerpt.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import Button from "./Button";
 import AudioSettingsContext from "../AudioSettingsContext";
 import axios from "axios";
