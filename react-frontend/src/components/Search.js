ca97b0b5dc02f5ca8b454cd8a624c66f8ce6bf06
chuck-stein <cfgstein@gmail.com>
2019-10-12 16:26:08 -0400

    convert record() to useEffect()

--- react-frontend/src/components/Search.js
+++ react-frontend/src/components/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import axios from 'axios';
 import AudioSettingsContext from "../AudioSettingsContext";
 import Button from "./Button";
@@ -6,48 +6,54 @@ import ExcerptFetch from "./ExcerptFetch";
 
 const Search = () => {
 
+    const [recording, setRecording] = useState(false);
     const [queryReady, setQueryReady] = useState(false);
     const [micQuery, setMicQuery] = useState([]);
     const audioSettingsContext = useContext(AudioSettingsContext);
 
-    const record = async () => {
-        const stream = await navigator.mediaDevices.getUserMedia({audio: true}); // TODO: handle denial of microphone permission
-        const context = new AudioContext();
-        const source = context.createMediaStreamSource(stream);
-        const processor = context.createScriptProcessor(4096, 1, 1);
-        source.connect(processor);
-        processor.connect(context.destination);
-        let buffers = [];
-        let samplesGathered = 0;
-        processor.onaudioprocess = (e) => { // TODO: update this deprecated script processor method to AudioWorklet
-            const downsampleContext = new OfflineAudioContext(
-                e.inputBuffer.numberOfChannels,
-                e.inputBuffer.duration * audioSettingsContext.sampleRate,
-                audioSettingsContext.sampleRate
-            );
-            const bufferSource = new AudioBufferSourceNode(downsampleContext, {
-                buffer: e.inputBuffer
-            });
+    useEffect(() => {
+
+        const downsample = async (buffer) => {
+            const downsampleContext = new OfflineAudioContext(buffer.numberOfChannels,
+                buffer.duration * audioSettingsContext.sampleRate, audioSettingsContext.sampleRate);
+            const bufferSource = new AudioBufferSourceNode(downsampleContext, { buffer: buffer });
             bufferSource.start(0);
             bufferSource.connect(downsampleContext.destination);
-            downsampleContext.startRendering().then((downsampledBuffer) => {
-                const bufferData = downsampledBuffer.getChannelData(0);
-                buffers.push(bufferData);
-                samplesGathered += bufferData.length;
-            });
-            if (samplesGathered >= audioSettingsContext.sampleRate * audioSettingsContext.excerptDuration) {
-                processor.disconnect();
-                let micData = [];
-                for (let buffer of buffers) {
-                    for (let sample of buffer) {
-                        micData.push(sample);
-                    }
-                }
-                setMicQuery(micData);
-                setQueryReady(true);
-            }
+            const downsampledBuffer = await downsampleContext.startRendering();
+            return downsampledBuffer.getChannelData(0);
         };
-    };
+
+        const record = async () => {
+            const stream = await navigator.mediaDevices.getUserMedia({audio: true}); // TODO: handle denial of microphone permission
+            const context = new AudioContext();
+            const source = context.createMediaStreamSource(stream);
+            const processor = context.createScriptProcessor(4096, 1, 1);
+            source.connect(processor);
+            processor.connect(context.destination);
+            let buffers = [];
+            let samplesGathered = 0;
+            processor.onaudioprocess = async (e) => { // TODO: update this deprecated script processor method to AudioWorklet (although AudioWorklet is only confirmed to work on Chrome, so deprecated method may still be best for compatibility
+                const downsampledBuffer = await downsample(e.inputBuffer);
+                buffers.push(downsampledBuffer);
+                samplesGathered += downsampledBuffer.length;
+                if (samplesGathered >= audioSettingsContext.sampleRate * audioSettingsContext.excerptDuration) {
+                    processor.disconnect();
+                    let micData = [];
+                    for (let buffer of buffers) {
+                        for (let sample of buffer) {
+                            micData.push(sample);
+                        }
+                    }
+                    setRecording(false);
+                    setMicQuery(micData);
+                    setQueryReady(true);
+                }
+            };
+        };
+
+        if (recording) record();
+
+    }, [recording, queryReady, micQuery, audioSettingsContext]);
 
     const searchAPICall = async () => {
         const response = await axios.post('/searchByMic', { searchQuery: micQuery });
@@ -57,7 +63,7 @@ const Search = () => {
     return queryReady ? (<ExcerptFetch apiCall={() => searchAPICall()} />) : (
         <div className={'search'}>
             <h2>Record any {audioSettingsContext.excerptDuration}-second sound to locate its corresponding excerpt:</h2>
-            <Button text='Start Recording' callback={() => record()} />
+            <Button text='Start Recording' callback={() => { if (!recording) setRecording(true) }} />
         </div>
     );
 
